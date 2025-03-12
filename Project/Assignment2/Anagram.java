40cace270f96e28545ba2d204aeb210e202b1604
Shristi Sahu <71396300+shristisahu@users.noreply.github.com>
2025-03-12 21:12:35 +0530

    Update Anagram.java

--- Project/Assignment2/Anagram.java
+++ Project/Assignment2/Anagram.java
@@ -1,12 +1,10 @@
-package practice;
-
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Anagram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter first String");
 		String input1 = sc.nextLine();
