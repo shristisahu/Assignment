9acc57fa5bf41753deafd4135b6d8f1eeb3b966b
Shristi Sahu <71396300+shristisahu@users.noreply.github.com>
2025-03-12 21:13:42 +0530

    Update Employee.java

--- Project/Assignment3/Employee.java
+++ Project/Assignment3/Employee.java
@@ -1,5 +1,3 @@
-package practice;
-
 import java.util.ArrayList;
 
 public class Employee {
@@ -59,7 +57,6 @@ public class Employee {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		DisplayDetails();
 
 	}
